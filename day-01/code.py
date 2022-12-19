329a6bfc0ebeed9a22e586f4784220a84aad1a22
Anish-Shobith <anish.shobith19@gmail.com>
2022-12-19 22:47:38 +0530

    Day01 - Fix

--- day-01/code.py
+++ day-01/code.py
@@ -3,7 +3,7 @@ b=int(input())
 for i in range(a, b+1):
     if i % 3 == 0:
         print('Foo')
-    elif i % 2 == 0:
+    elif i % 2 == 0 and i % 3 != 0:
         print('Bar')
-    elif i % 2 == 1:
+    elif i % 2 == 1 and i % 3 != 0:
         print('Baz')
