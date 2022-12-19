d85d7a839fe61ba978940ff12d78cd45f1630e48
Anish-Shobith <anish.shobith19@gmail.com>
2022-12-19 22:42:42 +0530

    Day 01 - Completed

---
+++ day-01/code.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+for i in range(a, b+1):
+    if i % 3 == 0:
+        print('Foo')
+    elif i % 2 == 0:
+        print('Bar')
+    elif i % 2 == 1:
+        print('Baz')
