1dd71334c53c1d1cc7e9eefd97afd078411c3620
Anish-Shobith <anish.shobith19@gmail.com>
2022-12-26 23:56:53 +0530

    Improve golfs

--- day-03/golfed.py
+++ day-03/golfed.py
@@ -1,1 +1,1 @@
-e=list(map(int,input().split()));[print(a,end=' ')for a in [x for x in e[1:]]if a>sum([x for x in e[1:]])/e[0]]
+e=list(map(int,input().split()));[print(a,end=' ')for a in[x for x in e[1:]]if a>sum([x for x in e[1:]])/e[0]]
